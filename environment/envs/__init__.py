1c08f6d0a465c41f893169f13f3f8f92f3c434f5
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2026-04-26 14:34:42 +0300

    add name register

---
+++ environment/envs/__init__.py
@@ -0,0 +1,29 @@
+from gymnasium import register, Env
+from gymnasium.envs.registration import EnvCreator
+
+from environment.envs.metrics import MetricResourceManagementEnvironment
+from environment.envs.metrics.metric_creator import MetricResourceManagementHomogeneousCreator
+
+class Generate(EnvCreator):
+
+    def __call__(self, **kwargs) -> Env:
+        creator = MetricResourceManagementHomogeneousCreator(
+            n_machines=kwargs["n_machines"],
+            n_jobs=kwargs["n_jobs"],
+            n_resources=kwargs["n_resources"],
+            n_ticks=kwargs["n_ticks"]
+        )
+        return MetricResourceManagementEnvironment(creator, render_mode=kwargs["render_mode"])
+
+
+register(
+    "resource-management-online",
+    Generate(),
+    kwargs=dict(
+        n_machines=1,
+        n_jobs=10,
+        n_resources=3,
+        n_ticks=5,
+        render_mode=None
+    )
+)
